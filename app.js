791a2e8a46cefc9309182bd4218358d2d73b9ccb
nandoparra <nando.parra.zapata@gmail.com>
2024-02-26 22:33:47 -0500

    mercado liebre

---
+++ app.js
@@ -0,0 +1,25 @@
+const express= require("express");
+const app= express();
+const path= require("path");
+const publicPath= path.resolve(__dirname, "./public")
+app.use(express.static(publicPath));
+app.listen(3060, ()=>{
+    console.log("servidor corriendo en el puerto 3060")
+});
+app.get("/", (req, res)=>{
+    res.sendFile((path.resolve(__dirname, "./views/home.html")))
+})
+app.get("/register", (req, res) => {
+	res.sendFile(path.resolve(__dirname, "./views/register.html"));
+});
+
+app.post("/register", (req, res) => {
+	res.sendFile(path.resolve(__dirname, "./views/home.html"));
+});
+app.get("/login", (req, res) => {
+	res.sendFile(path.resolve(__dirname, "./views/login.html"));
+});
+
+app.post("/login", (req, res) => {
+	res.sendFile(path.resolve(__dirname, "./views/home.html"));
+});

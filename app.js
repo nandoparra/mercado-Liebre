4146fa7c494ce25f7a919772de98a1fc4561ad56
nandoparra <nando.parra.zapata@gmail.com>
2024-02-27 19:45:21 -0500

    nuevos cambios

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express= require("express");
 const app= express();
 const path= require("path");
+
 const publicPath= path.resolve(__dirname, "./public")
+
 app.use(express.static(publicPath));
-app.listen(3060, ()=>{
-    console.log("servidor corriendo en el puerto 3060")
-});
+const port= process.env.PORT || 3060;
+app.listen(port, ()=>console.log(`Servidor corriendo en Puerto ${port}`));
+//app.listen(3060, ()=>{
+    //console.log("servidor corriendo en el puerto 3060")
+//});
 app.get("/", (req, res)=>{
     res.sendFile((path.resolve(__dirname, "./views/home.html")))
 })
